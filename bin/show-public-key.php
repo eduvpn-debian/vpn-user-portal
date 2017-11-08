3324242da8d22103ab62a138d974b16b86cf124b
Gijs Molenaar <gijs@pythonic.nl>
2017-11-08 09:49:35 +0100

    New upstream version 1.0.10

--- bin/show-public-key.php
+++ bin/show-public-key.php
@@ -7,10 +7,19 @@
  * Copyright: 2016-2017, The Commons Conservancy eduVPN Programme
  * SPDX-License-Identifier: AGPL-3.0+
  */
-require_once sprintf('%s/vendor/autoload.php', dirname(__DIR__));
+$baseDir = dirname(__DIR__);
+
+// find the autoloader (package installs, composer)
+foreach (['src', 'vendor'] as $autoloadDir) {
+    if (@file_exists(sprintf('%s/%s/autoload.php', $baseDir, $autoloadDir))) {
+        require_once sprintf('%s/%s/autoload.php', $baseDir, $autoloadDir);
+        break;
+    }
+}
 
 use SURFnet\VPN\Common\CliParser;
 use SURFnet\VPN\Common\FileIO;
+use SURFnet\VPN\Portal\SodiumCompat;
 
 try {
     $p = new CliParser(
@@ -27,9 +36,9 @@ try {
     }
 
     $instanceId = $opt->hasItem('instance') ? $opt->getItem('instance') : 'default';
-    $keyPairFile = sprintf('%s/data/%s/OAuth.key', dirname(__DIR__), $instanceId);
+    $keyPairFile = sprintf('%s/data/%s/OAuth.key', $baseDir, $instanceId);
     echo base64_encode(
-        \Sodium\crypto_sign_publickey(
+        SodiumCompat::crypto_sign_publickey(
             base64_decode(
                 FileIO::readFile($keyPairFile), true
             )
